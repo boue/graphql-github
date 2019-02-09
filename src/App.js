47d119711bf1830f29d4d089316e14d0c0398c76
boue <boue.germain@gmail.com>
2019-02-08 19:48:30 -0800

    Write the JSX for a form with an input of text and a submit button && add axios as dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
 
 const TITLE = 'React GraphQL Github Client';
 
@@ -7,6 +17,21 @@ class App extends Component {
     return (
       <div>
         <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input 
+            id="url"
+            type="text"
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <hr />
       </div>
     );
   }
